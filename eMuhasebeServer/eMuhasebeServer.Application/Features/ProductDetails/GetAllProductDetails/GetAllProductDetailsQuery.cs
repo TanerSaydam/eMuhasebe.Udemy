1f7a0c515fd536abe5c860ee3e7e71c5d94b4a8b
TanerSaydam <tanersaydam@gmail.com>
2024-04-29 00:27:01 +0300

    ProductDetail get all işlemi (Backend)

---
+++ eMuhasebeServer/eMuhasebeServer.Application/Features/ProductDetails/GetAllProductDetails/GetAllProductDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using eMuhasebeServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace eMuhasebeServer.Application.Features.ProductDetails.GetAllProductDetails;
+public sealed record GetAllProductDetailsQuery(
+    Guid ProductId) : IRequest<Result<Product>>;
---
+++ eMuhasebeServer/eMuhasebeServer.Application/Features/ProductDetails/GetAllProductDetails/GetAllProductDetailsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using eMuhasebeServer.Domain.Entities;
+using eMuhasebeServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace eMuhasebeServer.Application.Features.ProductDetails.GetAllProductDetails;
+
+internal sealed class GetAllProductDetailsQueryHandler(
+    IProductRepository productRepository) : IRequestHandler<GetAllProductDetailsQuery, Result<Product>>
+{
+    public async Task<Result<Product>> Handle(GetAllProductDetailsQuery request, CancellationToken cancellationToken)
+    {
+        Product? product = 
+            await productRepository
+            .Where(p => p.Id == request.ProductId)
+            .Include(p => p.Details)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if(product is null)
+        {
+            return Result<Product>.Failure("Ürün bulunamadı");
+        }
+
+        return product;
+    }
+}
